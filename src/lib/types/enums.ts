942a8a305daa1d71f4db16ac0e3a67995867ab9e
ChikoShidori <degnan200@gmail.com>
2022-04-08 20:48:16 +0100

    feat(search): now you can search for animes

--- src/lib/types/enums.ts
+++ src/lib/types/enums.ts
@@ -5,10 +5,12 @@ export type AvailableServicesURLSType = keyof typeof AvailableServicesURLS;
 
 export interface BaseElements {
 	LatestAnimes: string;
+	Search: string;
 }
 
 export const PageLocations: Record<AvailableServicesURLSType, BaseElements> = {
 	TioAnime: {
-		LatestAnimes: '#tioanime > div > section:nth-child(1) > ul'
+		LatestAnimes: '#tioanime > div > section:nth-child(1) > ul',
+		Search: '#tioanime > div > div.row.justify-content-between.filters-cont > main > ul'
 	}
 };
